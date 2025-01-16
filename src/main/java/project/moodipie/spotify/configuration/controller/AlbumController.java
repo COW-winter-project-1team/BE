c48db6edaaa855713bd55484d5fc702e67a45bab
baaamk <brandon55321@gmail.com>
2025-01-16 16:04:51 +0900

    feat: 트랙 검색 기능

--- src/main/java/project/moodipie/spotify/configuration/controller/AlbumController.java
+++ src/main/java/project/moodipie/spotify/configuration/controller/AlbumController.java
@@ -1,10 +1,12 @@
 package project.moodipie.spotify.configuration.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.moodipie.spotify.configuration.client.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/spotify/api")
@@ -32,16 +34,26 @@ public class AlbumController {
         return ResponseEntity.ok(response.getAlbums().getItems());
     }
 
-    @GetMapping("/tracks/{genre}")
-    public ResponseEntity<List<Track>> helloWorld2(@PathVariable("genre") String genre) {
+    @GetMapping("/tracks/{name}/{artist}")
+    public ResponseEntity<Track> helloWorld2(@PathVariable("name") String name,       // 곡 제목
+                                             @PathVariable("artist") String artist) {
         LoginRequest request = new LoginRequest(
                 "client_credentials",
                 "97bc0e5f8320421eaf8f9383ae3399be",
                 "f7e81d2bcfd04c9393925e0bfc4493f9"
         );
-        String query = "genre:" + genre;
+        String query = String.format("track:%s artist:%s", name, artist);
         String token = authSpotifyClient.login(request).getAccessToken();
-        TrackResponse response = trackSpotifyClient.getTrack("Bearer " + token, query,"track");
-        return ResponseEntity.ok(response.getTracks().getItems());
+        TrackResponse response = trackSpotifyClient.getTrack("Bearer " + token, query, "track");
+        Optional<Track> exactMatch = response.getTracks().getItems().stream()
+                .filter(track -> track.getAlbum().getName().equalsIgnoreCase(name))
+                .filter(track -> track.getAlbum().getArtists().stream()
+                        .anyMatch(a -> a.getName().equalsIgnoreCase(artist)))
+                .findFirst();
+
+        // 5. 결과 반환
+        return exactMatch
+                .map(ResponseEntity::ok) // 정확한 결과 반환
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
     }
 }

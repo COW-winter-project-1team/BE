8044a245696bfd908de53dbcb905c6d1ab026a95
Suehyun666 <em0200878@gmail.com>
2025-02-01 03:57:29 +0900

    fix 배포위한 수정

--- src/main/java/project/moodipie/music/playlist/service/PlaylistService.java
+++ src/main/java/project/moodipie/music/playlist/service/PlaylistService.java
@@ -3,7 +3,6 @@ package project.moodipie.music.playlist.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import project.moodipie.UserJpaRepository;
 import project.moodipie.entity.User;
 import project.moodipie.music.playlist.controller.dto.response.PlaylistResponse;
 import project.moodipie.music.playlist.entity.PlaylistTrack;
@@ -13,9 +12,9 @@ import project.moodipie.music.playlist.entity.Playlist;
 import project.moodipie.music.playlist.repository.PlaylistRepository;
 import project.moodipie.music.track.entity.Track;
 import project.moodipie.music.track.repository.TrackRepository;
+import project.moodipie.repository.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,7 +24,7 @@ public class PlaylistService {
     private final PlaylistRepository playlistRepository;
     private final TrackRepository trackRepository;
     private final PlaylistTrackRepository playlistTrackRepository;
-    private final UserJpaRepository userJpaRepository;
+    private final UserRepository userJpaRepository;
 
     public void savePlaylist(CreatePlaylistRequest request) {
         User user = userJpaRepository.findById(request.getUserId())

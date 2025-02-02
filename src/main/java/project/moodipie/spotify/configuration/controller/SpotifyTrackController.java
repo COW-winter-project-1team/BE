c52f930ef79204ac372de2a61f180317a5ed1541
baaamk <brandon55321@gmail.com>
2025-02-02 15:25:43 +0900

    refactor: 컨트롤러 명 변경

--- src/main/java/project/moodipie/spotify/configuration/controller/SpotifyTrackController.java
+++ src/main/java/project/moodipie/spotify/configuration/controller/SpotifyTrackController.java
@@ -1,6 +1,9 @@
 package project.moodipie.spotify.configuration.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +23,12 @@ public class SpotifyTrackController {
     private final SpotifyTrackService spotifyTrackService;
     private final TrackService trackService;
 
+    @Operation(summary = "외부 api로 노래 저장", description = "노래를 가져와 db에 저장합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "저장 성공"),
+    })
     @PostMapping("tracks/{name1}/{artist1}/{name2}/{artist2}")
-    public void SearchTracks(@PathVariable String artist1, @PathVariable String artist2, @PathVariable String name1, @PathVariable String name2) throws JsonProcessingException {
+    public void saveTracks(@PathVariable String artist1, @PathVariable String artist2, @PathVariable String name1, @PathVariable String name2) throws JsonProcessingException {
         List<String> trackNames = Arrays.asList(name1, name2);
         List<String> artistNames = Arrays.asList(artist1, artist2);
         List<CreateTrackRequest> tracks = spotifyTrackService.searchTracks(trackNames, artistNames);

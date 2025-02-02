01cda6723890540f3d0e2896fa0d2e404699a1a7
Suehyun666 <em0200878@gmail.com>
2025-02-03 01:09:28 +0900

    feat swager 추가 controller에 설명 추가했습니다.

--- src/main/java/project/moodipie/controller/HelpController.java
+++ src/main/java/project/moodipie/controller/HelpController.java
@@ -1,5 +1,8 @@
 package project.moodipie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,20 @@ import project.moodipie.dto.response.HelpResponse;
 @RestController
 @RequestMapping("/help")
 public class HelpController {
+    @Operation(summary = "도움말 페이지에서 넘어가기 ", description = "다음 도움말 페이지를 제공합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "다음 도움말 페이지"),
+    })
     @PostMapping
     public HelpResponse helpPost() {
         return new HelpResponse();
     }
+    
+    
+    @Operation(summary = "도움말 페이지", description = "첫 로그인 시 도움말 페이지를 제공합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "도움말 페이지"),
+    })
     @GetMapping
     public HelpResponse help() {
         return new HelpResponse();
--- src/main/java/project/moodipie/controller/UserController.java
+++ src/main/java/project/moodipie/controller/UserController.java
@@ -1,5 +1,8 @@
 package project.moodipie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
@@ -23,14 +26,20 @@ public class UserController {
     private final UserService userService;
     private final HttpSession session;
     private final UserRepository userRepository;
-
+    @Operation(summary = "마이페이지 조회", description = "내 정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "저장 성공"),
+    })
     @GetMapping("/users")//마이페이지
     public UserResponse userPage() {
         SessionUser currentUser = getSessionUser();
         UserResponse response = new UserResponse(currentUser.getUsername(), currentUser.getPicture());
         return response;
     }
-
+    @Operation(summary = "내 정보 수정", description = "내 정보를 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "수정 성공"),
+    })
     @PutMapping("/users")//회원정보 수정
     public void updateUser(@RequestBody UpdateUserRequest updateUserRequest) {
         SessionUser currentUser = getSessionUser();
@@ -38,24 +47,28 @@ public class UserController {
         User updateduser = userService.findUserByEmail(currentUser.getEmail());
         this.session.setAttribute("currentUser", new SessionUser(updateduser));
     }
-
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "탈퇴 성공"),
+    })
     @DeleteMapping("/users")//회원탈퇴
     public void deleteUser() {
         SessionUser currentUser = getSessionUser();
         userService.deleteUserByEmail(currentUser.getEmail());
         session.removeAttribute("currentUser");
     }
-
+    @Operation(summary = "회원가입", description = "회원에 가입합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+    })
     @PostMapping("/signup")//회원가입 기능
     public SignUpResponse signup(@RequestBody CreateUserRequest createUserRequest) {
         return userService.signup(createUserRequest);
     }
-
-    @GetMapping("/signup")//회원가입 페이지
-    public String signUpPage() {
-        return "signup";
-    }
-
+    @Operation(summary = "로그인", description = "내 정보로 로그인합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+    })
     @PostMapping("/login")//로그인
     public void login(@RequestBody LoginDto loginDto) {
         User loginuser = userService.login(loginDto);
@@ -68,21 +81,17 @@ public class UserController {
             session.setAttribute("currentUser", currentUser);// 기존 로그인일 때
         }
     }
-
-    @GetMapping("/login") // 로그인 페이지
-    public String getlogin() {
-        if (session.getAttribute("currentUser") != null) {
-            return "home"; //return "redirect:/home";
-        }
-        // 로그인 페이지로 이동
-        return "login";
-    }
-
+    @Operation(summary = "로그아웃", description = "로그아웃합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+    })
     @PostMapping("/logout")//로그아웃
     public void logout() {
         session.invalidate();
     }
 
+    
+    
     private SessionUser getSessionUser() {
         SessionUser currentUser = (SessionUser) session.getAttribute("currentUser");
         if (currentUser == null) {

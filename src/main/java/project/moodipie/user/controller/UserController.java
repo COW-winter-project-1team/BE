3dd132c2fffc9d62a99c88e13e4105de08a42a6a
baaamk <brandon55321@gmail.com>
2025-02-16 03:48:01 +0900

    refactor: 회원 수정 response 수정

--- src/main/java/project/moodipie/user/controller/UserController.java
+++ src/main/java/project/moodipie/user/controller/UserController.java
@@ -63,6 +63,7 @@ public class UserController {
             @AuthenticationPrincipal String userEmail,
             @Valid @RequestBody UpdateUserRequest updateUserRequest) {
         UserInfoResponse userInfoResponse = userService.updateUser(userEmail, updateUserRequest);
+
         ApiRes<UserInfoResponse> response = ApiRes.update(userInfoResponse);
         return ResponseEntity.status(response.getHttpStatus()).body(response);
     }

f35f360e86aa88ec9aca6755879edce2fd0afddc
baaamk <brandon55321@gmail.com>
2025-02-16 03:34:22 +0900

    refactor: UserInfoResponse 에서 password 제거

--- src/main/java/project/moodipie/user/controller/dto/response/UserInfoResponse.java
+++ src/main/java/project/moodipie/user/controller/dto/response/UserInfoResponse.java
@@ -16,14 +16,10 @@ public class UserInfoResponse {
     @Schema(description = "이메일", example = "moodipie@gmail.com")
     private String email;
 
-    @Schema(description = "password", example = "teamproject1!")
-    private String password;
-
     public static UserInfoResponse from(User user) {
         return UserInfoResponse.builder()
                 .username(user.getName())
                 .email(user.getEmail())
-                .password(user.getPassword())
                 .build();
 
     }

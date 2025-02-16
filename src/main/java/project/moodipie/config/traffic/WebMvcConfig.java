9ca1fd198eef2746ea8445496cf6798a23772b24
Suehyun666 <em0200878@gmail.com>
2025-02-17 02:41:00 +0900

    fix traffic 서비스 설정

---
+++ src/main/java/project/moodipie/config/traffic/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package project.moodipie.config.traffic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final HttpInterceptor httpInterceptor;
+
+    @Autowired
+    public WebMvcConfig(HttpInterceptor httpInterceptor) {
+        this.httpInterceptor = httpInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(httpInterceptor)
+                .addPathPatterns("/**");  // 모든 요청에 적용
+    }
+}
